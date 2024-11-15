4c5d088c5c554d3dccd3b2c73b4a2c028ed2faf5
patrick-cananthy <105574657+patrick-cananthy@users.noreply.github.com>
2024-11-15 12:08:28 -0800

    added the scroll effect to the index page

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,24 @@
 // Scroll Animation
 document.addEventListener("DOMContentLoaded", function() {
-  
-  const sections = document.querySelectorAll(".about-header, .governance-overview, .organizational-structure, .leadership-team, .history-overview, .timeline, .legacy, .management-card, .about-overview, .our-values, .our-mission");
+
+    const sections = document.querySelectorAll(".about-header, .governance-overview, .organizational-structure, .leadership-team, .history-overview, .timeline, .legacy, .management-card, .about-overview, .our-values, .our-mission,.contact,.aboutUs,.Services,.latest-news");
 
 
-  function checkInView() {
-    const triggerHeight = window.innerHeight / 1.3; // Trigger point for animation
+    function checkInView() {
+        const triggerHeight = window.innerHeight / 1.3; // Trigger point for animation
 
-    sections.forEach(section => {
-      const sectionTop = section.getBoundingClientRect().top;
-      
-      
-      if (sectionTop < triggerHeight) {
-        section.classList.add("show");
-      }
-    });
-  }
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
 
 
-  window.addEventListener("scroll", checkInView);
-  
-  checkInView();
+            if (sectionTop < triggerHeight) {
+                section.classList.add("show");
+            }
+        });
+    }
+
+
+    window.addEventListener("scroll", checkInView);
+
+    checkInView();
 });
